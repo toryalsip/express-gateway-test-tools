725a7575a4a8afe2fcbdd2be3bb50a29e8715649
jbalsip <13713268+jbalsip@users.noreply.github.com>
2018-01-29 16:03:42 -0500

    Use correct assertion for array size

--- test/server-tools/find-open-port.test.js
+++ test/server-tools/find-open-port.test.js
@@ -7,7 +7,7 @@ describe('server-tools', function () {
     it('should return a promise that resolves to an array of available ports', function () {
       return findOpenPortNumbers(3)
         .then((ports) => {
-          ports.length.should.be.equal(3);
+          ports.should.have.lengthOf(3);
         });
     });
   });
